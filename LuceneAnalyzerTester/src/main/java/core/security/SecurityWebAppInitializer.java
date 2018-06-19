7a1ed22b4ce045ee4660cc6185af4eca49136cb9
dzejdzej <jelenaa31051994@gmail.com>
2018-06-19 21:19:05 +0200

    initial import

--- LuceneAnalyzerTester/src/main/java/core/security/SecurityWebAppInitializer.java
+++
@@ -1,6 +0,0 @@
-package core.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
-}
---
+++ LuceneAnalyzerTester/src/main/java/rest/mvc/BenchmarkImporterController.java
@@ -0,0 +1,65 @@
+package rest.mvc;
+
+public class BenchmarkImporterController {
+	
+	/***
+	 * 
+	 * @Entity
+	 * BenchmarkImporter
+	 * 
+	 * POST
+	 * installNewBenchmarkImporter(MUltiPart part) {
+	 * 		String name = part.getParam("fileName");
+	 * 		Jar. ... 
+	 * 
+	 * 		ubacimo JAR na putanju na disku
+	 * ubacimo novi entiotet tipa BemnchmarkImporter u tabelu...novoi red
+	 * 
+	 * 
+	 * 
+	 * }
+	 * 
+	 * 
+	 */
+	
+	///////////////////
+	// Prosiriti benchmark controller
+	// importBenchmarks POST
+	//  NAVODI tipBenchMarkImportera koga treba koristiti
+	
+	/************
+	 * 
+	 *   na osnovu imena benchmarkImportera ,nadjemo ga iz baze
+	 *   BenchMarkImporter importer = benchmarkIMporterRespotiroty.findbyName(name);
+	 *   CLassloder load = ucitamo JAR fajl sa puitanje  importer.getPath();\
+	 *   
+	 *   Izvlacimo klasu BenchImporter iz jar-a
+	 *   BenchmarkImporter (interjfejs) imp = loader.findByClassName("BanchmarkImporter");
+	 *   
+	 *   importer.validate()
+	 *   importer.import()
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   
+	 * 
+	 * 
+	 */
+
+}
